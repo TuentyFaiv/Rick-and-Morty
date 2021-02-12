f286eb614776c8f4b2c4b6853b86c13a4ae9f8ca
Tonalli López <aguilablanca_7@live.com>
2021-02-12 17:13:07 -0600

    [Update] scroll restoration

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import '@styles/containers/layout.scss';
 import Header from '@components/Header';
 
-const Layout = ({ children }) => (
-  <>
-    <Header />
-    <main>{children}</main>
-    <footer>Footer</footer>
-  </>
-);
+const Layout = ({ children }) => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <footer>Footer</footer>
+    </>
+  );
+};
 
 export default Layout;
