9dab1ef6b1e5b751b8d394cca67e8ca26ba20eec
Tonalli López <aguilablanca_7@live.com>
2021-02-11 18:26:07 -0600

    [Add] Characters page and characters styles

---
+++ src/components/CardCharacters.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '@styles/components/cardCharacters.scss';
+
+const CardCharacters = (props) => (
+  <article className="cardCharacter">
+    <Link to={`/characters/${props.id}`}>
+      <img className="cardCharacter__image" src={props.image} alt={`${props.name} | ${props.location.dimension}`}/>
+      <h2 className="cardCharacter__title">
+        {props.name}
+        <span>{`${props.gender} - ${props.species}`}</span>
+      </h2>
+      <p><strong>Location: </strong>{props.location.name}</p>
+      <p><strong>Origin: </strong>{props.origin.dimension}</p>
+      <p><strong>Status: </strong>{props.status}</p>
+    </Link>
+  </article>
+);
+
+export default CardCharacters;
--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,7 +1,98 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useLazyQuery } from '@apollo/client';
+import { CHARACTERS } from '@queries';
 
-const Characters = () => (
-  <h1>Characters</h1>
-);
+import '@styles/pages/characters.scss';
+import CardCharacters from '@components/CardCharacters';
+
+const Characters = () => {
+  const [query, { loading, data, error }] = useLazyQuery(CHARACTERS);
+  const [variables, setVariables] = useState({
+    page: 1,
+    filter: { name: '', status: '', species: '', type: '', gender: '' },
+  });
+  const searchRef = useRef(null);
+
+  const prev = data?.characters.info?.prev;
+  const prevPage = prev === null ? '' : prev === undefined ? 'Loading...' : prev;
+  const next = data?.characters.info?.next;
+  const nextPage = next === null ? '' : next === undefined ? 'Loading...' : next;
+
+  const handlePrev = () => {
+    setVariables({
+      ...variables,
+      page: data?.characters.info?.prev,
+    });
+    window.scrollTo(0, 0);
+  };
+
+  const handleNext = () => {
+    setVariables({
+      ...variables,
+      page: data?.characters.info?.next,
+    });
+    window.scrollTo(0, 0);
+  };
+
+  const handleSearchByName = () => {
+    setVariables({
+      ...variables,
+      filter: {
+        ...variables.filter,
+        name: searchRef.current.value,
+      },
+    });
+  };
+
+  useEffect(() => {
+    query({ variables });
+  }, [variables]);
+
+  return (
+    <section className="characters">
+      <div className="characters__header">
+        <h1 className="characters__header-title">
+          {
+            `Total characters: ${!loading ? data?.characters.info?.count : !error ? 'loading...' : ''} 
+            | Pages: ${!loading ? data?.characters.info?.pages : !error ? 'loading...' : ''}`
+          }
+        </h1>
+        <input
+          ref={searchRef}
+          type="text"
+          className="characters__header-search"
+          placeholder="Search by name"
+          value={variables.filter.name}
+          onChange={handleSearchByName}
+        />
+        <div className="characters__prevandnext">
+          <button type="button" onClick={handlePrev} disabled={data?.characters.info?.prev ? false : true}>
+            {`Prev page ${prevPage}`}
+          </button>
+          <button type="button" onClick={handleNext} disabled={data?.characters.info?.next ? false : true}>
+            {`Next page ${nextPage}`}
+          </button>
+        </div>
+      </div>
+      <div className="characters__content">
+        {loading && <h1>Loading...</h1>}
+        {error && <h1>{error.message}</h1>}
+        {
+          data?.characters.results?.map((character) => (
+            <CardCharacters key={character.id} {...character} />
+          ))
+        }
+      </div>
+      <div className="characters__prevandnext">
+        <button type="button" onClick={handlePrev} disabled={data?.characters.info?.prev ? false : true}>
+          {`Prev page ${prevPage}`}
+        </button>
+        <button type="button" onClick={handleNext} disabled={data?.characters.info?.next ? false : true}>
+          {`Next page ${nextPage}`}
+        </button>
+      </div>
+    </section>
+  );
+};
 
 export default Characters;
